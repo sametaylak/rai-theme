605610c7778f7399d6210bdfc645d483e6ff051c
GitHub Action <action@github.com>
2022-04-23 12:59:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/sametaylak/raitheme/MyBundle.kt
+++ src/main/kotlin/com/github/sametaylak/raitheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sametaylak.raitheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sametaylak/raitheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sametaylak/raitheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sametaylak.raitheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sametaylak.raitheme.MyBundle
 
 class MyApplicationService {
 
